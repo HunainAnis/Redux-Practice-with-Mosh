613b1f00f2002d29aa331151f4617ce97db64664
Hunain Anis <hunainmuhammad@rocketmail.com>
2020-04-29 23:50:27 +0500

    Updated wrapInDiv( )  to wrap( )

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ let output = '<div>' + input.trim() + '<div>';
 
 const trim = str => str.trim();
 const wrapInDiv = str => `<div>${str}</div>`;
+const wrap = type => str => `<${type}>${str}</${type}>`;
 const toLowerCase = str => str.toLowerCase();
 
 let result = toLowerCase(wrapInDiv(trim(input)));
 
 // const transform = compose(toLowerCase, wrapInDiv, trim);
-
-const transform = pipe(trim, wrapInDiv, toLowerCase);
+// const transform = pipe(trim, wrapInDiv, toLowerCase);
+const transform = pipe(trim, wrap('span'), toLowerCase);
 
 result = transform(input);
 
