f2b67cedb4aa3073328a7f01852daadc56a495fd
Hunain Anis <hunainmuhammad@rocketmail.com>
2020-04-29 23:13:11 +0500

    Initial Functional programming

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+let input = '   JavaScript   ';
+let output = '<div>' + input.trim() + '<div>';
+
+const trim = str => str.trim();
+const wrapInDiv = str => `<div>${str}</div>`;
+const toLowerCase = str => str.toLowerCase();
+
+const result = toLowerCase(wrapInDiv(trim(input)));
+
+console.log(result)
